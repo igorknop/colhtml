cdc13b6fde0793759e76d128d26c9fd1daf4c1ec
Igor Knop <igorknop@gmail.com>
2018-05-06 14:44:15 -0300

    Add Guard and Warrior classes

--- BuildersHall.js
+++ BuildersHall.js
@@ -1,6 +1,9 @@
 import {D} from "./Config.js"
 import {Sprite} from "./Sprite.js"
 import {Builder} from "./Builder.js"
+import {Farmer} from "./Farmer.js"
+import {Warrior} from "./Warrior.js"
+import {Guard} from "./Guard.js"
 
 class BuildersHall extends Sprite{
   constructor(world,x,y){
@@ -13,14 +16,20 @@ class BuildersHall extends Sprite{
       switch (this.world.creating) {
         case D.A_WARRIORS:
           this.cd = D.PEOPLE_WARRIOR_SPAWN_CD
+          var walker = new Warrior(this.world,this.x, this.y)
           break;
         case D.A_GUARDS:
           this.cd = D.PEOPLE_GUARD_SPAWN_CD
+          var walker = new Guard(this.world,this.x, this.y)
+          break;
+        case D.A_FARMERS:
+          this.cd = D.PEOPLE_FARMER_SPAWN_CD
+          var walker = new Farmer(this.world,this.x, this.y)
           break;
         default:
           this.cd = D.PEOPLE_BUILDER_SPAWN_CD
+          var walker = new Builder(this.world,this.x, this.y)
       }
-      var walker = new Builder(this.world,this.x, this.y)
       if (walker) this.world.people.push(walker)
     }
   }
