9c32d3473724034a4a37649e38cac77a584f5582
Igor Knop <igorknop@gmail.com>
2018-05-06 18:01:38 -0300

    Add basic restart and next level and scoring

--- World.js
+++ World.js
@@ -6,6 +6,7 @@ import {BuildersHall} from "./BuildersHall.js"
 
 class World {
   constructor() {
+    this.state = 0
     this.dt = 0
     this.prev = 0
     this.raf
@@ -21,9 +22,14 @@ class World {
     this.image.src = "0x72_16x16DungeonTileset.v4.png"
     this.image2 = new Image()
     this.image2.src = "tileset-pokemon_dawn.png"
+    this.restartCD = 0;
+    this.nextLevelCD = 0;
+    this.level = 1
   }
 
-  setup() {
+  setup(l=1) {
+    this.level = l
+    this.state = 0
     this.dt = 0
     this.prev = 0
     this.raf
@@ -32,20 +38,81 @@ class World {
     this.buildings = []
     this.monsters = []
     this.people = []
-    this.food = 50
+    this.food = 50-this.level*5
     this.foodcd = 0
+    this.restartCD = 0;
+    this.nextLevelCD = 0;
 
     this.people.push(new Builder(this, 150, 150))
-    this.monsters.push(new Monster(this, 20, 20))
+    for (var i = 0; i < this.level; i++) {
+      this.monsters.push(new Monster(this, 40*i+10,20))
+    }
   }
 
   step(dt, ctx) {
     for (var i = 0; i < 300; i += 32) {
       for (var j = 0; j < 300; j += 32) {
         //ctx.drawImage(this.image2, 368,192,32,32,i,j,32,32)
-        ctx.drawImage(this.image2, 368,241,32,32,i,j,32,32)
+        ctx.drawImage(this.image2, 368, 241, 32, 32, i, j, 32, 32)
       }
     }
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 300, 28)
+    ctx.strokeRect(0, 0, 300, 28)
+
+    if (this.playerLost()) {
+      ctx.fillStyle = 'red'
+      ctx.strokeStyle = 'black'
+      ctx.font = '32px Impact'
+      ctx.fillText('You LOSE!', 100, 100);
+      ctx.strokeText('You LOSE!', 100, 100);
+      ctx.fillStyle = 'white'
+      ctx.font = '18px Impact'
+      let score = this.score()
+      ctx.fillText('Score: ' + score, 100, 150);
+      ctx.strokeText('Score: ' + score, 100, 150);
+      ctx.fillText('Restart in: '+Math.floor(this.restartCD), 100, 180);
+      ctx.strokeText('Restart in: '+Math.floor(this.restartCD), 100, 180);
+      if (this.state === 0) {
+        this.state = 1
+        this.restartCD = 3
+        return
+      } else {
+        this.restartCD -= dt
+        if (this.restartCD <= 0) {
+          this.setup()
+          this.state = 0
+        }
+        return
+      }
+    }
+    if (this.playerWon()) {
+      ctx.fillStyle = 'yellow'
+      ctx.strokeStyle = 'black'
+      ctx.font = '32px Impact'
+      ctx.fillText('You WIN!', 100, 100);
+      ctx.strokeText('You WIN!', 100, 100);
+      let score = this.score()
+      ctx.fillText('Score: ' + score, 100, 150);
+      ctx.strokeText('Score: ' + score, 100, 150);
+      ctx.fillStyle = 'white'
+      ctx.font = '18px Impact'
+      ctx.fillText('Next level in: '+Math.floor(this.nextLevelCD), 100, 180);
+      ctx.strokeText('Next level in: '+Math.floor(this.nextLevelCD), 100, 180);
+      if (this.state === 0) {
+        this.state = 1
+        this.nextLevelCD = 3
+        return
+      } else {
+        this.nextLevelCD -= dt
+        if (this.nextLevelCD <= 0) {
+          this.setup(this.level+1)
+          this.state = 0
+        }
+        return
+      }
+    }
+
     for (var i = this.people.length - 1; i >= 0; i--) {
       if (this.food <= 0 && this.foodcd <= 0) {
         this.people.splice(i, 1)
@@ -101,6 +168,35 @@ class World {
         }
       }
     }
+  }
+  playerLost() {
+    if (this.people.length == 0) {
+      return true
+    }
+  }
+  playerWon() {
+    if (this.monsters.length == 0) {
+      return true
+    }
+  }
+  score() {
+    var colors = ['red', 'green', 'blue', 'yellow']
+    var hist = {
+      'red': 0,
+      'green': 0,
+      'blue': 0,
+      'yellow': 0
+    }
+    for (let i = 0; i < this.people.length; i++) {
+      hist[this.people[i].color]++
+    }
+    let score = 101
+    for (let i = 0; i < colors.length; i++) {
+      if (hist[colors[i]] < score) {
+        score = hist[colors[i]]
+      }
+    }
+    return score
   }
 }
 export {
